9c7d81284cf81cb7e43f891a89990297405340e4
skanes17 <Scottskanes@gmail.com>
2022-10-29 13:46:58 -0230

    game component importing properly

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,10 @@
 import React from "react";
+import Game from "./Game";
 
 export default function App() {
-  return <div>show me this</div>;
+  return (
+    <div>
+      <Game name="Scott" />
+    </div>
+  );
 }
---
+++ src/components/Game.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+// @ts-ignore
+export default function Game(props) {
+  return (
+    <div>
+      <h1>Welcome to {props.name}'s game.</h1>
+    </div>
+  );
+}
