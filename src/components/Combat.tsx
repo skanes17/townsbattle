9f211b1f607f86902c5e2e88689bf5cad425300d
skanes17 <Scottskanes@gmail.com>
2022-11-02 12:47:01 -0230

    placeholder generation friendly and enemy units

--- src/components/Combat.tsx
+++ src/components/Combat.tsx
@@ -8,21 +8,45 @@ export default function Combat(props) {
     props.setTurn(props.turn + 1);
   }
 
-  const [melee, setMelee] = useState({
+  // friendlies -- optimize later
+  const [melee1, setMelee1] = useState({
     attack: 2,
     defense: 2,
     name: "Melee",
     type: "balanced",
     quantity: 0,
   });
-  const [pewpew, setPewpew] = useState({
+  const [pewpew1, setPewpew1] = useState({
     attack: 3,
     defense: 1,
     name: "Pewpew",
     type: "ranged",
     quantity: 0,
   });
-  const [tanky, setTanky] = useState({
+  const [tanky1, setTanky1] = useState({
+    attack: 1,
+    defense: 3,
+    name: "Tanky",
+    type: "defensive",
+    quantity: 0,
+  });
+
+  // enemies -- optimize later
+  const [melee2, setMelee2] = useState({
+    attack: 2,
+    defense: 2,
+    name: "Melee",
+    type: "balanced",
+    quantity: 0,
+  });
+  const [pewpew2, setPewpew2] = useState({
+    attack: 3,
+    defense: 1,
+    name: "Pewpew",
+    type: "ranged",
+    quantity: 0,
+  });
+  const [tanky2, setTanky2] = useState({
     attack: 1,
     defense: 3,
     name: "Tanky",
@@ -36,44 +60,39 @@ export default function Combat(props) {
     setUnit({
       ...unit, // copy other fields
       quantity: unit.quantity + 1,
-      /*     setMelee({
-      ...melee, // copy other fields
-      quantity: melee.quantity + 1, */
     });
-
-    /* setMelee(melee.quantity + 1); */
   }
 
   return (
     <>
-      <h2>You are in combat. It's super fun and you love it.</h2>
+      <h2>You are in combat.</h2>
       <div className="friendlyUnits" style={{ fontWeight: "bold" }}>
         Friendly Units:
       </div>
       <div>
         <div>
-          {melee.name} ({melee.quantity} units)
+          {melee1.name} ({melee1.quantity} units)
         </div>
         <div>
-          Atk {melee.attack} Def {melee.defense}
-        </div>
-      </div>
-      <br></br>
-      <div>
-        <div>
-          {pewpew.name} ({pewpew.quantity} units)
-        </div>
-        <div>
-          Atk {pewpew.attack} Def {pewpew.defense}
+          Atk {melee1.attack} Def {melee1.defense}
         </div>
       </div>
       <br></br>
       <div>
         <div>
-          {tanky.name} ({tanky.quantity} units)
+          {pewpew1.name} ({pewpew1.quantity} units)
         </div>
         <div>
-          Atk {tanky.attack} Def {tanky.defense}
+          Atk {pewpew1.attack} Def {pewpew1.defense}
+        </div>
+      </div>
+      <br></br>
+      <div>
+        <div>
+          {tanky1.name} ({tanky1.quantity} units)
+        </div>
+        <div>
+          Atk {tanky1.attack} Def {tanky1.defense}
         </div>
       </div>
 
@@ -81,14 +100,14 @@ export default function Combat(props) {
 
       <div className="trainFriendlyUnits">
         {/* Took forever to figure this out */}
-        <button onClick={() => trainUnit(melee, setMelee)}>
-          Train {melee.name}
+        <button onClick={() => trainUnit(melee1, setMelee1)}>
+          Train {melee1.name}
         </button>
-        <button onClick={() => trainUnit(pewpew, setPewpew)}>
-          Train {pewpew.name}
+        <button onClick={() => trainUnit(pewpew1, setPewpew1)}>
+          Train {pewpew1.name}
         </button>
-        <button onClick={() => trainUnit(tanky, setTanky)}>
-          Train {tanky.name}
+        <button onClick={() => trainUnit(tanky1, setTanky1)}>
+          Train {tanky1.name}
         </button>
       </div>
       <br></br>
@@ -98,43 +117,45 @@ export default function Combat(props) {
       </div>
       <div>
         <div>
-          {melee.name} ({melee.quantity} units)
+          {melee2.name} ({melee2.quantity} units)
         </div>
         <div>
-          Atk {melee.attack} Def {melee.defense}
-        </div>
-      </div>
-      <br></br>
-      <div>
-        <div>
-          {pewpew.name} ({pewpew.quantity} units)
-        </div>
-        <div>
-          Atk {pewpew.attack} Def {pewpew.defense}
+          Atk {melee2.attack} Def {melee2.defense}
         </div>
       </div>
       <br></br>
       <div>
         <div>
-          {tanky.name} ({tanky.quantity} units)
+          {pewpew2.name} ({pewpew2.quantity} units)
         </div>
         <div>
-          Atk {tanky.attack} Def {tanky.defense}
+          Atk {pewpew2.attack} Def {pewpew2.defense}
+        </div>
+      </div>
+      <br></br>
+      <div>
+        <div>
+          {tanky2.name} ({tanky2.quantity} units)
+        </div>
+        <div>
+          Atk {tanky2.attack} Def {tanky2.defense}
         </div>
       </div>
 
       <div className="trainEnemyUnits">
         {/* Took forever to figure this out */}
-        <button onClick={() => trainUnit(melee, setMelee)}>
-          Train {melee.name}
+        <button onClick={() => trainUnit(melee2, setMelee2)}>
+          Train {melee2.name}
         </button>
-        <button onClick={() => trainUnit(pewpew, setPewpew)}>
-          Train {pewpew.name}
+        <button onClick={() => trainUnit(pewpew2, setPewpew2)}>
+          Train {pewpew2.name}
         </button>
-        <button onClick={() => trainUnit(tanky, setTanky)}>
-          Train {tanky.name}
+        <button onClick={() => trainUnit(tanky2, setTanky2)}>
+          Train {tanky2.name}
         </button>
       </div>
+
+      {/* TODO: Implement direct combat between units, w/randomization etc */}
 
       <br></br>
       <button onClick={endTurn}>End Turn</button>
