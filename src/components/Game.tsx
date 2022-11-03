570342e214379c1cf28a0b3b8355e12117e655ab
skanes17 <Scottskanes@gmail.com>
2022-11-03 21:15:05 -0230

    trying to generate new units as objects

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import Planning from "./Planning";
 import Combat from "./Combat";
+import { setConstantValue } from "typescript";
 
 // @ts-ignore
 export default function Game(props) {
@@ -25,13 +26,85 @@ export default function Game(props) {
   const [stoneMultiplier, setStoneMultipler] = useState(1);
   const [ironMultiplier, setIronMultipler] = useState(1);
 
+  // initialized an array, will practice adding units to this
+  /*   const [myUnits, setMyUnits] = useState([
+    {
+      melee: {
+        attack: 5,
+        defense: 5,
+        id: 0,
+      },
+    },
+  ]); */
+
+  // @ts-ignore
+  /*   function unitObjectToAddToArray(type, attack, defense, id) {
+    // template for a unit object
+  } */
+
+  // this should add new units to the old array
+  // @ts-ignore
+  /*   setMyUnits((myUnits) => {
+    return [...myUnits, ...newUnit];
+  }); */
+
+  /*   const existingUnits = [
+    {
+      melee: {
+        attack: 5,
+        defense: 5,
+        id: 0,
+      },
+    },
+    {
+      pewpew: {
+        attack: 7,
+        defense: 3,
+        id: 1,
+      },
+    },
+  ];
+  console.log(existingUnits);
+
+  const newUnit = [
+    {
+      tanky: {
+        attack: 3,
+        defense: 7,
+        id: 2,
+      },
+    },
+  ];
+
+  const merged = [...existingUnits, ...newUnit];
+  console.log("This is the merged array...");
+  console.log(merged); */
+
+  /* TODO: think on Functional updates for adding units
+  setMyUnits(myUnits => {
+    // Object.assign would also work
+    return {...myUnits, ...updatedValues};
+  }) */
+
+  /* TEMPLATE
+  const [friendlyUnits, setFriendlyUnits] = useState({
+    melee: {
+      attack: 5,
+      defense: 5,
+      name: "Melee",
+      type: "balanced",
+      quantity: 0,
+      id: 0,
+    },
+  }); */
+
   return (
     <div>
       <h1>Welcome to the game.</h1>
       <div style={{ fontWeight: "bold" }}>Turn Number: {turn}</div>
 
-      {/*  <Combat /> */}
-      {turn === combatTurn ? (
+      <Combat />
+      {/* {turn === combatTurn ? (
         <Combat turn={turn} setTurn={setTurn} setCombatTurn={setCombatTurn} />
       ) : (
         <Planning
@@ -60,7 +133,7 @@ export default function Game(props) {
           ironMultiplier={ironMultiplier}
           setIronMultipler={setIronMultipler}
         />
-      )}
+      )} */}
     </div>
   );
 }
