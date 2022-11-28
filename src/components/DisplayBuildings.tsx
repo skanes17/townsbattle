ac22bc0968384efb1a9edd4a9d500f1ce86848d3
skanes17 <Scottskanes@gmail.com>
2022-11-28 12:59:48 -0330

    tried styling buildings using Grid

--- src/components/DisplayBuildings.tsx
+++ src/components/DisplayBuildings.tsx
@@ -17,10 +17,60 @@ export default function DisplayBuildings({ buildings }: DisplayBuildingsProps) {
   return (
     <div>
       <div className="font-bold">Buildings Constructed</div>
+      <div className="flex space-x-2">
+        {constructedBuildings.map((buildingType) => (
+          <div className="bg-white text-black w-48 h-72 border-4 border-blue-900 rounded-md shadow-md shadow-gray-500/50 grid grid-cols-3 gap-1">
+            <div className="flex justify-center items-center h-8 bg-indigo-200 text-lg font-bold mx-2 rounded-b-md col-span-3">
+              {buildings[buildingType].name}
+            </div>
+            <div className="flex justify-center items-center h-16 col-span-3">
+              <img
+                className="mx-auto w-16 h-16"
+                src={townCenter}
+                alt={`building`}
+              />
+            </div>
+            <div className="mx-4 p-1 h-28 shadow-sm bg-amber-100 border-slate-300 rounded-sm col-span-3">
+              {buildings[buildingType].effect === "" ? (
+                <>
+                  <span className="font-bold">Effect </span>
+                  <span>None</span>
+                </>
+              ) : (
+                <>
+                  <span className="font-bold pr-0.5">Effect </span>
+                  <span>{buildings[buildingType].effect}</span>
+                </>
+              )}
+            </div>
+
+            {/* TODO: How to get these spaced properly?? */}
+
+            <div></div>
+            <div className="bg-slate-800 text-md mt-4 px-1 rounded-t-lg text-white justify-self-center place-self-end">
+              Tier {buildings[buildingType].tier}
+            </div>
+            {/* TODO: Replace number for health with heart symbols */}
+            <div className="bg-slate-800 text-md mt-4 px-1 rounded-tl-lg text-white place-self-end">
+              ❤️{buildings[buildingType].health}
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+/* With Flex
+  
+  <div>
+      <div className="font-bold">Buildings Constructed</div>
       <div className="flex">
         {constructedBuildings.map((buildingType) => (
-          <div className="bg-white text-black w-48 p-2 rounded-md shadow-md flex flex-col items-center">
-            <div className="p-0.5">{buildings[buildingType].name}</div>
+          <div className="bg-white text-black w-48 p-2 rounded-md shadow-md shadow-gray-500/50 flex flex-col items-center">
+            <div className="p-0.5 font-bold">
+              {buildings[buildingType].name}
+            </div>
             <div className="flex-1 border-slate-300"></div>
             <div className="p-0.5 h-16">
               <img
@@ -43,15 +93,12 @@ export default function DisplayBuildings({ buildings }: DisplayBuildingsProps) {
               )}
             </div>
 
-            {/* TODO: How to get these spaced properly?? */}
             <div className="flex flex-row justify-between">
               <span>Tier {buildings[buildingType].tier}</span>
-              {/* TODO: Replace number for health with heart symbols */}
               <span>❤️{buildings[buildingType].health}</span>
             </div>
           </div>
         ))}
       </div>
     </div>
-  );
-}
+  */
