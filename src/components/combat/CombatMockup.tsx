f1cb5e37f5ce5a8acf3e7faa77d2dbe3cfb34609
skanes17 <Scottskanes@gmail.com>
2022-12-12 09:49:21 -0330

    grid layout mocked for 1280px and up

--- src/components/combat/CombatMockup.tsx
+++ src/components/combat/CombatMockup.tsx
@@ -8,12 +8,13 @@ export default function CombatMockup() {
     // grid for the whole page
     <body className="grid min-h-screen grid-cols-12 grid-rows-4 place-content-stretch gap-2 p-4">
       {/* sub grid to control squares for units -- use different sizes per screen*/}
-      <div className="square col-span-4 col-start-1 row-span-2 row-start-1 mx-auto grid w-full max-w-sm grid-flow-col self-center bg-blue-500">
-        <div className="square w-full bg-red-700">M</div>
-        <div className="square w-full bg-blue-700">P</div>
-        <div className="square w-full bg-red-700">M</div>
-        <div className="square w-full bg-red-700">M</div>
-        <div className="square w-full bg-green-700">T</div>
+      {/* columns/rows could be conditionally rendered by unit count (eg 5x5 for 25 units) */}
+      <div className="square col-span-4 col-start-1 row-span-2 row-start-1 mx-auto grid w-full max-w-sm grid-flow-row-dense self-center bg-blue-500">
+        <div className="square max-w-min bg-red-700">M</div>
+        <div className="square max-w-min bg-blue-700">P</div>
+        <div className="square max-w-min bg-red-700">M</div>
+        <div className="square max-w-min bg-red-700">M</div>
+        <div className="square max-w-min bg-green-700">T</div>
       </div>
       <div className="col-span-4 col-start-5 row-span-2 row-start-1 aspect-[5/3] w-full grid-flow-row self-center rounded-md bg-gray-500 p-4">
         Log
@@ -21,16 +22,19 @@ export default function CombatMockup() {
       <div className="square col-span-4 col-start-9 row-span-2 row-start-1 mx-auto grid w-full max-w-sm grid-flow-col self-center bg-red-500">
         Enemies
       </div>
-      <div className="card col-span-5 col-start-1 row-span-3 row-start-3 w-4/5 max-w-xs justify-self-end rounded-md bg-blue-400 px-4">
+      <div className="card col-span-5 col-start-1 row-span-3 row-start-3 mr-4 w-4/5 max-w-xs justify-self-end rounded-md bg-blue-400 px-4">
         Friendly Card
       </div>
 
-      <div className="col-span-4 col-start-5 row-start-3 w-1/2 place-self-center p-4 text-center text-lg">
-        <p>Possible flavour text here, sometimes.</p>
+      <div className=" md:text-md col-span-4 col-start-5 row-start-3 flex h-full w-1/2 items-center place-self-center bg-gray-200/10 p-2 text-center text-xs sm:text-sm lg:text-lg xl:text-xl">
+        <p>
+          Possible flavour text here, sometimes. Maybe. It depends. What'd you
+          like?
+        </p>
       </div>
 
       <div className="col-span-2 col-start-6 row-start-4 mx-auto p-4">
-        <div className="flex justify-center pb-4">
+        <div className="h-100% flex justify-center pb-4">
           <Button buttonColor="blue" onClick={() => ""}>
             Fight!
           </Button>
@@ -42,7 +46,7 @@ export default function CombatMockup() {
         </div>
       </div>
 
-      <div className="card col-span-5 col-start-8 row-span-3 row-start-3 w-4/5 max-w-xs justify-self-start rounded-md bg-red-400 px-4">
+      <div className="card col-span-5 col-start-8 row-span-3 row-start-3 ml-4 w-4/5 max-w-xs justify-self-start rounded-md bg-red-400 px-4">
         Enemy Card
       </div>
     </body>
