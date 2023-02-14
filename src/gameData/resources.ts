ee30873268f759f2bd4fc98ce2fae17bded09325
skanes17 <Scottskanes@gmail.com>
2023-02-14 13:44:39 -0330

    Added thoughts on a resources refactor

--- src/gameData/resources.ts
+++ src/gameData/resources.ts
@@ -40,3 +40,53 @@ export const resourceData = {
     description: "Collects 1🔩.",
   },
 };
+
+/*
+// TODO: Consider a new approach:
+export const resourceData = {
+  collected: {
+    freeworkers: 5,
+    wood: 0,
+    stone: 0,
+    metal: 0,
+  },
+  details: {
+    freeworkers: {
+      name: "Freeworker",
+      resourceSymbol: "🛠️",
+      description:
+        "Used when gathering resources, training units, and constructing buildings.",
+    },
+    wood: {
+      name: "Wood",
+      resourceSymbol: "🪵",
+      workers: 0,
+      workersNeeded: 1,
+      workerName: "Woodcutters",
+      workerType: "woodcutters",
+      workerSymbol: "🪓",
+      description: "Collects 1🪵.",
+    },
+    stone: {
+      name: "Stone",
+      resourceSymbol: "🪨",
+      workers: 0,
+      workersNeeded: 1,
+      workerName: "Stonemasons",
+      workerType: "stonemasons",
+      workerSymbol: "⚒️",
+      description: "Collects 1🪨.",
+    },
+    metal: {
+      name: "Metal",
+      resourceSymbol: "🔩",
+      workers: 0,
+      workersNeeded: 1,
+      workerName: "Metalworkers",
+      workerType: "metalworkers",
+      workerSymbol: "🥽",
+      description: "Collects 1🔩.",
+    },
+  },
+};
+*/
