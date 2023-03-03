e993e03c1cabdfa3b2963c7982718717ad7d9016
skanes17 <Scottskanes@gmail.com>
2023-03-03 12:19:08 -0330

    more tweaks

--- src/components/cards/worker/WorkerCardDescription.tsx
+++ src/components/cards/worker/WorkerCardDescription.tsx
@@ -14,8 +14,9 @@ export default function WorkerCardDescription({
   const bg = bgImage;
 
   return (
+    // TODO: Get a good width!
     <div
-      className={`${bg} m-2 flex aspect-square items-center rounded-lg border-zinc-900/50 bg-contain`}
+      className={`${bg} m-2 flex aspect-square w-32 items-center rounded-lg border-zinc-900/50 bg-contain`}
     >
       <div className="m-auto w-3/4 rounded-sm border-slate-300 bg-slate-300/90 p-2 text-center text-sm font-semibold text-black opacity-0 shadow-md backdrop-grayscale transition-all ease-in-out group-hover:opacity-100 group-active:opacity-100">
         Gather {resources[resourceType].multiplier}{" "}
