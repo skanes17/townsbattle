ee6e339896221680cddb0585ba1058569a7af2d4
skanes17 <Scottskanes@gmail.com>
2023-03-05 16:08:48 -0330

    center hoverText using grid

--- src/components/cards/CardBgWithImage.tsx
+++ src/components/cards/CardBgWithImage.tsx
@@ -13,7 +13,7 @@ export default function CardBgWithImage({
 
   return (
     <div
-      className={`${bg} h-52 w-52 rounded-lg border border-transparent bg-wood bg-cover bg-center bg-no-repeat`}
+      className={`${bg} grid h-52 w-52 grid-rows-5 rounded-lg border border-transparent bg-wood bg-cover bg-center bg-no-repeat`}
     >
       {children}
     </div>
