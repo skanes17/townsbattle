ea426509f6b9acd783f074d8f0c17af510adbc09
skanes17 <Scottskanes@gmail.com>
2023-03-10 22:45:14 -0330

    Made equality turn 5

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -448,7 +448,7 @@ export default function Game(props: GameProps) {
     const growthRate = 0.05; // tweak if necessary for balance but, generally, don't touch!
     // TODO: could auto-set this at start of the game based on difficulty
     // Equality turn is APPROXIMATE number of combat turns to pass, on normal mode, until power levels of both armies are about equal
-    const equalityTurn = 10; // default 10
+    const equalityTurn = 5; // default 5
 
     // set a baseline power level for the game -- tweak for balance as necessary
     const basePowerLevel = 4;
