be5cc1319f4b537086817918327e77e7253b1d57
skanes17 <Scottskanes@gmail.com>
2023-03-11 19:53:41 -0330

    Center game subtitle

--- src/components/startPage/MenuTitle.tsx
+++ src/components/startPage/MenuTitle.tsx
@@ -11,7 +11,7 @@ export default function MenuTitle({ title, subtitle }: MenuTitleProps) {
       <div className="text-center text-5xl sm:text-6xl md:text-7xl lg:text-8xl xl:text-9xl">
         {title}
       </div>
-      <div className="text-xl sm:text-2xl md:text-3xl lg:text-4xl">
+      <div className="text-center text-xl sm:text-2xl md:text-3xl lg:text-4xl">
         {subtitle}
       </div>
     </div>
