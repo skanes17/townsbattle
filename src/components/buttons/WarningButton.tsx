84d5a1891a0ec9837788c3177848d851bd5f5612
skanes17 <Scottskanes@gmail.com>
2023-03-14 22:59:59 -0230

    WarningButton bug fix

--- src/components/buttons/WarningButton.tsx
+++ src/components/buttons/WarningButton.tsx
@@ -20,7 +20,7 @@ export default function WarningButton({
       break;
     case 1:
       buttonText = "Are you SURE?";
-      buttonStyle = `animate-jiggleSmall bg-[#9E1A1A] font-bold `;
+      buttonStyle = `animate-jiggleSmall bg-[#9E1A1A] font-bold`;
       break;
     case 2:
       buttonText = "DELETE";
@@ -32,7 +32,7 @@ export default function WarningButton({
       break;
     default:
       buttonText = "⭐ All Saved Data Deleted ⭐";
-      buttonStyle = `bg-red-600 font-semibold`;
+      buttonStyle = `bg-green-800 font-semibold`;
       break;
   }
 
@@ -42,10 +42,11 @@ export default function WarningButton({
         type="button"
         className={`${buttonStyle} w-full flex-1 rounded-md border p-2.5 outline-transparent ring-offset-2 transition-transform ease-in `}
         onClick={
-          counter !== 3
+          counter < 2
             ? () => incrementPopupCounter()
             : () => {
                 localStorage.clear();
+                incrementPopupCounter();
               }
         }
       >
