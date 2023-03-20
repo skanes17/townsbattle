661c77b97d53759d897f15bc38f774af940234e5
skanes17 <Scottskanes@gmail.com>
2023-03-20 12:08:40 -0230

    replace router content w/ createRoutesFromElements

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,36 +1,25 @@
-import { createBrowserRouter } from "react-router-dom";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+} from "react-router-dom";
 import Game from "./Game";
 import { About, HowToPlay, Leaderboards, Options, Play } from "./startPage";
 import SharedStartMenu from "./startPage/SharedStartMenu";
 
-export const router = createBrowserRouter([
-  {
-    path: "/",
-    /* parent Route /, SharedStartMenu is rendered for all children using Outlet */
-    element: <SharedStartMenu />,
-    children: [
-      /* TODO: Make a New Game and Load Game, with Load Game disabled if no saves */
-      {
-        path: "play",
-        element: <Play />,
-      },
-      {
-        path: "leaderboards",
-        element: <Leaderboards />,
-      },
-      {
-        path: "options",
-        element: <Options />,
-      },
-      {
-        path: "howtoplay",
-        element: <HowToPlay />,
-      },
-      {
-        path: "about",
-        element: <About />,
-      },
-    ],
-  },
-  { path: "/play/game", element: <Game /> },
-]);
+export const router = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+      {/* parent Route /, SharedStartMenu is rendered for all children using Outlet */}
+      <Route path="/" element={<SharedStartMenu />}>
+        {/* TODO: Make a New Game and Load Game, with Load Game disabled if no saves */}
+        <Route path="play" element={<Play />} />
+        <Route path="leaderboards" element={<Leaderboards />} />
+        <Route path="options" element={<Options />} />
+        <Route path="howtoplay" element={<HowToPlay />} />
+        <Route path="about" element={<About />} />
+      </Route>
+      <Route path="/play/game" element={<Game />} />
+    </>
+  )
+);
