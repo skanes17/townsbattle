7d199841c58b280ef696f749fb7226dead93d28a
skanes17 <Scottskanes@gmail.com>
2023-03-20 11:11:51 -0230

    remove BrowserRouter basename prop

--- src/index.tsx
+++ src/index.tsx
@@ -9,7 +9,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="React-game">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
