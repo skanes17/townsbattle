0b7e902233750d562f9f7f86cae01ed5bacfc059
skanes17 <Scottskanes@gmail.com>
2023-03-23 13:45:20 -0230

    Fix main menu font size

--- src/tailwindStyles/RouterLinkStyle.ts
+++ src/tailwindStyles/RouterLinkStyle.ts
@@ -1,2 +1,2 @@
 export const routerLinkStyle =
-  "flex w-3/5 border-2 border-white/20 justify-start rounded-md bg-gradient-to-r from-black/70 to-black/70 p-1 text-base backdrop-blur-sm hover:bg-gradient-to-r hover:from-white/40 hover:to-white/20 hover:backdrop-blur-sm hover:border-black active:scale-95 sm:text-xl md:p-2 md:text-2xl lg:text-3xl xl:p-3 xl:text-4xl hover:font-semibold hover:text-black";
+  "flex w-3/5 border-2 border-white/20 justify-start rounded-md bg-gradient-to-r from-black/70 to-black/70 p-1 text-base backdrop-blur-sm hover:bg-gradient-to-r hover:from-white/40 hover:to-white/20 hover:backdrop-blur-sm hover:border-black active:scale-95 sm:text-xl md:p-2 md:text-2xl lg:text-3xl xl:p-3 xl:text-4xl hover:font-normal hover:text-black";
