424a2cd333201715d4a8d1d34314c9b44b54f841
skanes17 <Scottskanes@gmail.com>
2023-03-30 19:01:43 -0230

    Fix SaveFileLink grid issue

--- src/components/startPage/SaveFileLink.tsx
+++ src/components/startPage/SaveFileLink.tsx
@@ -23,26 +23,28 @@ export function SaveFileLink({ gameSave }: SaveFileLinkProps) {
   };
 
   return (
-    <div className="m-1 grid grid-cols-[9fr_1fr] gap-2">
-      {counter < 2 && (
-        <Link
-          className="rounded-md border-2 border-white/5 bg-blue-800/50 p-2 text-white outline-transparent ring-white ring-offset-1 hover:border-white/50 hover:bg-blue-800/75 focus:ring-1"
-          to={`/${gameSave.gameId}`}
-        >
-          <SaveFileData gameSave={gameSave} />
-        </Link>
-      )}
+    <>
       {counter < 3 && (
-        <button
-          type="button"
-          className="w-24 place-self-center rounded-md border-2 border-white/5 bg-red-700 p-2 font-math text-white outline-transparent ring-white ring-offset-1 hover:border-white/50 hover:bg-red-800 focus:ring-1"
-          onClick={() => handleClick()}
-        >
-          {counter === 0 && "✖"}
-          {counter === 1 && "For sure?"}
-          {counter === 2 && "Deleted!"}
-        </button>
+        <div className="m-1 grid grid-cols-[9fr_1fr] gap-2">
+          {counter < 2 && (
+            <Link
+              className="rounded-md border-2 border-white/5 bg-blue-800/50 p-2 text-white outline-transparent ring-white ring-offset-1 hover:border-white/50 hover:bg-blue-800/75 focus:ring-1"
+              to={`/${gameSave.gameId}`}
+            >
+              <SaveFileData gameSave={gameSave} />
+            </Link>
+          )}
+          <button
+            type="button"
+            className="w-24 place-self-center rounded-md border-2 border-white/5 bg-red-700 p-2 font-math text-white outline-transparent ring-white ring-offset-1 hover:border-white/50 hover:bg-red-800 focus:ring-1"
+            onClick={() => handleClick()}
+          >
+            {counter === 0 && "✖"}
+            {counter === 1 && "For sure?"}
+            {counter === 2 && "Deleted!"}
+          </button>
+        </div>
       )}
-    </div>
+    </>
   );
 }
